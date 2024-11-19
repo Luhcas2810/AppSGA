d2b869f0bdaba66617e98d6a3b01969ce6048e99
AlephariM <p.m.alejandro.2015@gmail.com>
2024-11-19 02:30:18 -0500

    Agrega bd Alejandro

--- CapaConexion/ConexionSQL.cs
+++ CapaConexion/ConexionSQL.cs
@@ -9,7 +9,8 @@ namespace CapaConexion
     public class ConexionSQL
     {
         //public static string conexionSQL = "Data Source=proyectosapm.database.windows.net;Initial Catalog=AppSGA;User ID=administrador;Password=1234qwer+II;Trusted_Connection=False;";
-        public static string conexionSQL = "Data Source=PC-JESUS;Initial Catalog=AppSGA;Integrated Security=True;";
+        //public static string conexionSQL = "Data Source=PC-JESUS;Initial Catalog=AppSGA;Integrated Security=True;";
+        public static string conexionSQL = @"Server=LAPTOP-VK7CU80I\SERVIDOR_SQL;Database=AppSGA;Integrated Security=True;";
 
     }
 }
